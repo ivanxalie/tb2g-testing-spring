89e3101c6427a3f1e3bf3937c479078e87875693
ivanxalie <ivanxalie@gmail.com>
2025-03-19 12:32:21 +0300

    Add YannyPropertiesWordProducer and test for it

---
+++ src/main/java/org/springframework/samples/petclinic/sfg/impl/YannyPropertiesWordProducer.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.sfg.impl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("externalized")
+public class YannyPropertiesWordProducer implements WordProducer {
+    private String word;
+
+    @Value("${say.word}")
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    @Override
+    public String word() {
+        return word;
+    }
+}
