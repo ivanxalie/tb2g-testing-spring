f97f883879515484b96ef88f0e43f8cad49329e8
ivanxalie <ivanxalie@gmail.com>
2025-03-20 23:36:33 +0300

    Add full coverage to org.springframework.samples.petclinic.web.OwnerController.processFindForm

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -3,11 +3,16 @@ package org.springframework.samples.petclinic.web;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.samples.petclinic.web.OwnerController.VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -38,5 +43,41 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("owner"))
                 .andExpect(view().name(VIEWS_OWNER_CREATE_OR_UPDATE_FORM));
+    }
+
+    @Test
+    void findByNameNotFound() throws Exception {
+        mockMvc.perform(get("/owners")
+                        .param("lastName", "Donnt find me!"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
+    }
+
+    @Test
+    void findByNameReturnList() throws Exception {
+        given(clinicService.findOwnerByLastName(""))
+                .willReturn(List.of(new Owner(), new Owner()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("selections"))
+                .andExpect(view().name("owners/ownersList"));
+
+        then(clinicService).should().findOwnerByLastName("");
+    }
+
+    @Test
+    void findByNameReturnSingle() throws Exception {
+        Owner single = new Owner();
+        single.setId(1);
+        given(clinicService.findOwnerByLastName("Alex"))
+                .willReturn(List.of(single));
+
+        mockMvc.perform(get("/owners")
+                        .param("lastName", "Alex"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/" + single.getId()));
+
+        then(clinicService).should().findOwnerByLastName("Alex");
     }
 }
